7d6ea2f234c84793e38acb76343fa8a4fd757f0a
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-10 01:43:44 -0400

    endpoints connected to router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model');
+
+const router = express.Router();
+
+router.get('/', async (req,res) => {
+    const posts = await Posts.find();
+    res.status(200).json(posts);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express'); // implement your server here
-  // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');  // require your posts router and connect it here
+
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter)
 
 server.get('/', (req,res) => {
     res.send(`
-        <h1>from express server! :)</h1>
+        <h1>Hello from express! :)</h1>
     `)
 });
 
