2a65aeb2a0bd3b5863435be404db4e3ec4b91147
Fry-Fr <ryjlwdg89@gmail.com>
2021-07-10 01:11:05 -0400

    initialize server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express'); // implement your server here
+  // require your posts router and connect it here
+const server = express();
+
+server.get('/', (req,res) => {
+    res.send(`
+        <h1>from express server! :)</h1>
+    `)
+});
+
+module.exports = server;
